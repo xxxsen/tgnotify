8393eee07de7f56d878f891d290633cb63a23744
xxxtest <xxxsen@example.com>
2021-03-22 20:08:47 +0800

    feat: add latest tag

--- build_docker.sh
+++ build_docker.sh
@@ -6,3 +6,6 @@ docker buildx create --use --name mybuild
 
 docker buildx build -t xxxsen/tgnotify:0.0.1 \
   --platform=linux/amd64,linux/arm64 . --push
+
+docker buildx build -t xxxsen/tgnotify:latest \
+  --platform=linux/amd64,linux/arm64 . --push
