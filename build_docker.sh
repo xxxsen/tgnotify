d9c367239bbfb03f9b2e4688e8fef3f76c96c7c1
xxxtest <xxxtest@xx.co>
2021-12-18 17:51:58 +0800

    chore: tag

--- build_docker.sh
+++ build_docker.sh
@@ -4,5 +4,5 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker buildx create --use --name mybuild
 
-docker buildx build -t xxxsen/tgnotify:0.0.2 -t xxxsen/tgnotify:latest  \
+docker buildx build -t xxxsen/tgnotify:v0.0.2 -t xxxsen/tgnotify:latest  \
   --platform=linux/amd64,linux/arm64 . --push
